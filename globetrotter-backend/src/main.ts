515707fd10a3cd28e3360f63feadd2c3c130d07c
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-01 03:59:51 +0000

    add swagger

--- globetrotter-backend/src/main.ts
+++ globetrotter-backend/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger, ZodValidationPipe } from 'nestjs-zod';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setVersion('1.0')
+    .setTitle('Globetrotter')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    useGlobalPrefix: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
+void patchNestJsSwagger();
 void bootstrap();
