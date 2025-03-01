5bbe05fb40cae5ab57a27a272604292ba9149d59
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-01 16:58:46 +0000

    data mining

--- globetrotter-backend/src/env.ts
+++ globetrotter-backend/src/env.ts
@@ -3,6 +3,3 @@ import 'dotenv/config';
 export const NODE_ENV = process.env.NODE_ENV;
 export const MONGO_URI = process.env.MONGO_URI as string;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
-
-export const GEMINI_API_KEY = process.env.GEMINI_API_KEY as string;
-export const DEEPSEEK_API_KEY = process.env.DEEPSEEK_API_KEY as string;
