f8efcf60a349a03e67027f2845c91cd9973c4b36
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-01 06:05:26 +0000

    add homepage

--- globetrotter-frontend/src/app/page.tsx
+++ globetrotter-frontend/src/app/page.tsx
@@ -1,3 +1,31 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <main>
+      <div className="h-dvh">
+        <Image
+          className="absolute h-full w-full object-cover"
+          src={'/static/images/background.webp'}
+          alt="Backgorund"
+          width={2600}
+          height={1600}
+        />
+        <div className="relative flex h-full w-full flex-col items-center justify-center gap-2">
+          <h1 className="heading text-8xl text-white italic">Globetrotter</h1>
+          <h2 className="heading text-2xl text-white">
+            Unlock the World's Hidden Wonders
+          </h2>
+          <button
+            className="rounded-lg px-4 py-2 text-xl font-medium text-white italic shadow"
+            style={{
+              background: 'var(--primary-gradient)',
+            }}
+          >
+            Play now
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
